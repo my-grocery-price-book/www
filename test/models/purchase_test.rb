0a0fa3387f5b2e5ea7b2a7c4176da1072eb1b27c
Concetta Isolano <concetta.isolano@gmail.com>
2015-06-23 11:58:55 +0200

    Add annotate gem

--- test/models/purchase_test.rb
+++ test/models/purchase_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id           :integer          not null, primary key
+#  purchased_on :date
+#  store        :string
+#  location     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  shopper_id   :integer
+#
+
 require 'test_helper'
 
 class PurchaseTest < ActiveSupport::TestCase
