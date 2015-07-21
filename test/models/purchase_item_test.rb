fbd1d3b60ef37546a12decbec84e587ccbf3d16c
Concetta Isolano <concetta.isolano@gmail.com>
2015-07-21 10:53:09 +0200

    Indentation

--- test/models/purchase_item_test.rb
+++ test/models/purchase_item_test.rb
@@ -18,26 +18,28 @@ require 'test_helper'
 
 class PurchaseItemTest < ActiveSupport::TestCase
   context 'validation' do
-    should 'not be valid if product_brand_name not uniq per purchase' do
-      purchase = purchases(:grant_purchase)
-      PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
-      purchase_item = PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
-      assert_not_empty purchase_item.errors[:product_brand_name]
-    end
+    context 'product_brand_name'
+      should 'not be valid if not uniq per purchase' do
+        purchase = purchases(:grant_purchase)
+        PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
+        purchase_item = PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
+        assert_not_empty purchase_item.errors[:product_brand_name]
+      end
 
-    should 'be valid if product_brand_name uniq per purchase' do
-      purchase = purchases(:grant_purchase)
-      PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Woolworths Bread')
-      purchase_item = PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
-      assert_empty purchase_item.errors[:product_brand_name]
-    end
+      should 'be valid if uniq per purchase' do
+        purchase = purchases(:grant_purchase)
+        PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Woolworths Bread')
+        purchase_item = PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
+        assert_empty purchase_item.errors[:product_brand_name]
+      end
 
-    should 'be valid if same product_brand_name but different purchase' do
-      purchase = purchases(:grant_purchase)
-      purchase1 = purchases(:one)
-      PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
-      purchase_item = PurchaseItem.create(purchase_id: purchase1.id, product_brand_name: 'Sasko Bread')
-      assert_empty purchase_item.errors[:product_brand_name]
+      should 'be valid if same but different purchase' do
+        purchase = purchases(:grant_purchase)
+        purchase1 = purchases(:one)
+        PurchaseItem.create(purchase_id: purchase.id, product_brand_name: 'Sasko Bread')
+        purchase_item = PurchaseItem.create(purchase_id: purchase1.id, product_brand_name: 'Sasko Bread')
+        assert_empty purchase_item.errors[:product_brand_name]
+      end
     end
   end
 end
