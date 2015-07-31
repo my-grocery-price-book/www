207f5d6d1b6c7b308aed9967603d84b3f118eb3f
Grant Speelman <grant.speelman@ubxd.com>
2015-07-31 21:07:10 +0200

    create ENV['DATABASE_URL'] from RDS ENV variables for AWS

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 # Load the Rails application.
+if ENV['RDS_DB_NAME'] && ENV['RDS_USERNAME']
+  ENV['DATABASE_URL'] = "postgresql://#{ENV['RDS_USERNAME']}:#{ENV['RDS_PASSWORD']}:#{ENV['RDS_HOSTNAME']}/#{ENV['RDS_DB_NAME']}?pool=5"
+end
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
