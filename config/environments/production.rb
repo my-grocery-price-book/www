01ee12627bbd352ba3b9154913e2dae492826d89
Grant Speelman <grant.speelman@ubxd.com>
2015-09-05 19:48:39 +0200

    fix production mailer default_url_options host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'my-grocery-price-book.co.za' }
+  config.action_mailer.default_url_options = { host: 'www.my-grocery-price-book.co.za' }
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for mailgun
