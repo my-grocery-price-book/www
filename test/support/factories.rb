5c7ae67d0da147f192dbbc7889afb17b4b8393e8
Grant Speelman <grant.speelman@ubxd.com>
2015-10-18 13:41:47 +0200

    rubocop fixes

--- test/support/factories.rb
+++ test/support/factories.rb
@@ -1,8 +1,8 @@
 # @param [Hash] shopper_args
 # @return [Shopper]
 def create_shopper(shopper_args = {})
-  Shopper.create!({password: '123123123',
-                   password_confirmation: '123123123',
-                   email: 'email@example.com',
-                   confirmed_at: Time.now}.merge(shopper_args))
+  Shopper.create!({ password: '123123123',
+                    password_confirmation: '123123123',
+                    email: 'email@example.com',
+                    confirmed_at: Time.current }.merge(shopper_args))
 end
