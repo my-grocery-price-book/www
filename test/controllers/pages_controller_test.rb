b23347043c9c494b440ce1ae9b615b27f537e767
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-02-04 15:35:31 +0200

    typo

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-§  test 'should get index' do
+  test 'should get index' do
     get :index
     assert_response :success
   end
