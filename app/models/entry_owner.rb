b6375132d93a541ccd2ee525c96b9df3bec0f968
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-06-23 09:47:35 +0200

    return method price_entry_product_name that was being used by local_suggestions

--- app/models/entry_owner.rb
+++ app/models/entry_owner.rb
@@ -13,11 +13,17 @@ class EntryOwner < ActiveRecord::Base
   validates :price_entry_id, :shopper_id, presence: true
   belongs_to :price_entry
 
+  def price_entry_product_name
+    price_entry.product_name
+  end
+
   # @param [Shopper] shopper
   # @return [Array<String>]
   def self.local_suggestions(shopper:)
     entry_owners = includes(:price_entry).where(shopper_id: shopper.id).order('id DESC').limit(1000)
-    suggestions = entry_owners.collect(&:price_entry_product_name)
+    suggestions = entry_owners.collect do |entry|
+      entry.price_entry_product_name
+    end
     suggestions.sort!
     suggestions.uniq!
     suggestions
