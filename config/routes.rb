d26c8dbbbdecdf0c0e93817695e9391bf0d7e188
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-07-09 13:14:39 +0200

    rake rails:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'contact' => 'pages#contact'
   get 'thank_you' => 'pages#thank_you'
