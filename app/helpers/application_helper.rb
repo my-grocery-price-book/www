71d8d10e559912f58cee73945ce4a495fe7af4a2
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-08-02 22:58:43 +0200

    currently not using embedded_svg due to new logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,16 +7,16 @@ module ApplicationHelper
     MultiJson.load(render(args.merge(formats: [:json])))
   end
 
-  def embedded_svg(filename, options = {})
-    # Cache the result of the parsing svg for next render
-    # rubocop:disable Rails/OutputSafety
-    Rails.cache.fetch("embedded_#{filename}_#{options.hash}") do
-      file = File.read(Rails.root.join('app', 'assets', 'images', filename))
-      doc = Nokogiri::HTML::DocumentFragment.parse file
-      svg = doc.at_css 'svg'
-      svg['class'] = options[:class] if options[:class].present?
-      doc.to_html
-    end.html_safe
-    # rubocop:enable Rails/OutputSafety
-  end
+  # def embedded_svg(filename, options = {})
+  #   # Cache the result of the parsing svg for next render
+  #   # rubocop:disable Rails/OutputSafety
+  #   Rails.cache.fetch("embedded_#{filename}_#{options.hash}") do
+  #     file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+  #     doc = Nokogiri::HTML::DocumentFragment.parse file
+  #     svg = doc.at_css 'svg'
+  #     svg['class'] = options[:class] if options[:class].present?
+  #     doc.to_html
+  #   end.html_safe
+  #   # rubocop:enable Rails/OutputSafety
+  # end
 end
