ee637e59bbec11e44b5f91bb7aed05f178608bb6
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-08-07 08:51:58 +0200

    use puma in feature tests and save_screenshot if content missing

--- test/features_helper.rb
+++ test/features_helper.rb
@@ -12,6 +12,13 @@ end
 
 Capybara.default_driver = :poltergeist
 
+require "puma"
+Capybara.register_server("puma") do |app, port|
+  server = Puma::Server.new(app)
+  server.add_tcp_listener(Capybara.server_host, port)
+  server.run
+end
+
 class PersonaSession
   include Capybara::DSL
   include Capybara::Email::DSL
@@ -27,6 +34,18 @@ class PersonaSession
     link_path = email.find_link(link_name)[:href].gsub("http://#{host}", '')
     visit link_path
   end
+
+  def has_content_with_screenshot?(*args)
+    if has_content_without_screenshot?(*args)
+      true
+    else
+      save_screenshot
+      false
+    end
+  end
+
+  alias_method :has_content_without_screenshot?, :has_content?
+  alias_method :has_content?, :has_content_with_screenshot?
 
   def perform(&block)
     instance_exec(&block)
