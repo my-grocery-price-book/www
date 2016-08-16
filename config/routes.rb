1dd4901ecf53bc88b5de9da38532567280507bbc
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-08-16 22:45:24 +0200

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -36,13 +36,10 @@ Rails.application.routes.draw do
     resources :items, controller: 'shopping_list_items', only: [:index, :create]
   end
 
-  resources :shopping_list_items, only: [] do
+  resources :shopping_list_items, only: [:update, :destroy] do
     collection do
       get 'latest'
     end
-  end
-
-  resources :shopping_list_items, only: [:update, :destroy] do
     resource :purchases, controller: 'shopping_list_item_purchases', only: [:create, :destroy]
   end
 
