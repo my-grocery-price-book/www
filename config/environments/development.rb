10b6f4d8e0abba0298889051e03c681ad413bf91
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-09-07 18:27:07 +0200

    use groc.dev instead of groc-dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'www.groc-dev' }
+  config.action_mailer.default_url_options = { host: 'www.groc.dev' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
