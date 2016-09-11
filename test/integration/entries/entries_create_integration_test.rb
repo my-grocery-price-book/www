a7355c36322e4a1235c609f8c8d0656d00fbfff1
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-09-12 00:01:36 +0200

    flay: reduce duplication in EntriesIntegrationTest tests

--- test/integration/entries/entries_create_integration_test.rb
+++ test/integration/entries/entries_create_integration_test.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 require 'test_helper'
+require_relative 'entries_integration_helper'
 
-class EntriesPostIntegrationTest < IntegrationTest
+class EntriesCreateIntegrationTest < EntriesIntegrationTest
   setup do
-    @shopper = create_shopper
-    login_shopper(@shopper)
-    @price_book = PriceBook.create!(shopper: @shopper)
-    @page = PriceBook::Page.create!(book: @price_book, name: 'Beans', category: 'Cupboard Food', unit: 'grams')
+    sign_in_and_create_page
+  end
+
+  def store
+    @store ||= Store.create!(name: 'Test', location: 'New', region_code: 'ZAR-WC')
   end
 
   context 'POST /books/:book_id/pages/:page_id/entries' do
     context 'success' do
       setup do
-        @store = Store.create!(name: 'Test', location: 'New', region_code: 'ZAR-WC')
-        post "/books/#{@price_book.to_param}/pages/#{@page.to_param}/entries",
-             params: { price_entry: { date_on: '2016-04-27', store_id: @store.id,
+        post "/books/#{price_book.to_param}/pages/#{book_page.to_param}/entries",
+             params: { price_entry: { date_on: '2016-04-27', store_id: store.id,
                                       product_name: 'Cans of Coke', amount: '1',
                                       package_size: '340', total_price: '5.59' } }
         assert_response :redirect
@@ -22,7 +23,7 @@ class EntriesPostIntegrationTest < IntegrationTest
 
       should 'save the record' do
         entry = PriceEntry.last.attributes.except('id', 'old_id', 'old_store_id', 'created_at', 'updated_at')
-        assert_equal({ 'date_on' => Date.parse('2016-04-27'), 'store_id' => @store.id,
+        assert_equal({ 'date_on' => Date.parse('2016-04-27'), 'store_id' => store.id,
                        'product_name' => 'Cans of Coke', 'amount' => 1, 'package_size' => 340,
                        'total_price' => 5.59, 'package_unit' => 'grams' },
                      entry)
@@ -31,22 +32,22 @@ class EntriesPostIntegrationTest < IntegrationTest
       should 'assign entry to shopper' do
         entry = PriceEntry.last
 
-        assert_includes(EntryOwner.entries_for_shopper(@shopper), entry)
+        assert_includes(EntryOwner.entries_for_shopper(shopper), entry)
       end
 
       should 'adds product_name to page' do
-        @page.reload
-        assert_includes(@page.product_names, 'Cans of Coke')
+        book_page.reload
+        assert_includes(book_page.product_names, 'Cans of Coke')
       end
 
       should 'redirect to price_page_path' do
-        assert_redirected_to(book_page_path(@price_book, @page))
+        assert_redirected_to(book_page_path(price_book, book_page))
       end
     end
 
     context 'validation errors' do
       should 'render the edit form with errors' do
-        post "/books/#{@price_book.to_param}/pages/#{@page.to_param}/entries",
+        post "/books/#{price_book.to_param}/pages/#{book_page.to_param}/entries",
              params: { price_entry: { date_on: '', store_id: '', product_name: '', amount: '',
                                       package_size: '', total_price: '' } }
         assert_response :success
--- test/integration/entries/entries_update_integration_test.rb
+++ test/integration/entries/entries_update_integration_test.rb
@@ -1,56 +1,21 @@
 # frozen_string_literal: true
 require 'test_helper'
+require_relative 'entries_integration_helper'
 
-class EntriesIntegrationTest < IntegrationTest
-  attr_reader :current_page
-  attr_reader :current_price_book
-  attr_reader :current_shopper
-
+class EntriesEditIntegrationTest < EntriesIntegrationTest
   setup do
-    @current_shopper = FactoryGirl.create(:shopper)
-    login_shopper(@current_shopper)
-    @current_price_book = PriceBook.create!(shopper: @current_shopper)
-    @current_page = FactoryGirl.create(:page, book: @current_price_book)
+    sign_in_and_create_page
   end
 
   def base_url
-    "/books/#{current_price_book.to_param}/pages/#{current_page.to_param}/entries"
-  end
-
-  context 'GET /books/:book_id/pages/:page_id/entries/:id/edit' do
-    context 'own entry' do
-      should 'show edit page' do
-        entry = FactoryGirl.create(:price_entry)
-        EntryOwner.create!(price_entry: entry, shopper: current_shopper)
-        get "#{base_url}/#{entry.to_param}/edit"
-        assert_response :success
-        assert response.body.include?('Edit')
-      end
-    end
-
-    context 'other shoppers entry' do
-      should 'should raise ActiveRecord::RecordNotFound' do
-        entry = FactoryGirl.create(:price_entry)
-        assert_raises(ActiveRecord::RecordNotFound) do
-          get "#{base_url}/#{entry.to_param}/edit"
-        end
-      end
-    end
-
-    context 'none existing book' do
-      should 'should raise ActiveRecord::RecordNotFound' do
-        assert_raises(ActiveRecord::RecordNotFound) do
-          get "#{base_url}/0/edit"
-        end
-      end
-    end
+    "/books/#{price_book.to_param}/pages/#{book_page.to_param}/entries"
   end
 
   context 'PUT /books/:book_id/pages/:page_id/entries/:id' do
     context 'own entry' do
       setup do
         @entry = FactoryGirl.create(:price_entry)
-        EntryOwner.create!(price_entry: @entry, shopper: current_shopper)
+        EntryOwner.create!(price_entry: @entry, shopper: shopper)
       end
 
       context 'validation failed' do
@@ -73,7 +38,7 @@ class EntriesIntegrationTest < IntegrationTest
         should 'redirects' do
           put "#{base_url}/#{@entry.to_param}",
               params: { price_entry: { product_name: 'New Name' } }
-          assert_redirected_to book_page_path(current_price_book, current_page)
+          assert_redirected_to book_page_path(price_book, book_page)
         end
 
         should 'update entry' do
@@ -109,4 +74,5 @@ class EntriesIntegrationTest < IntegrationTest
       end
     end
   end
+
 end
