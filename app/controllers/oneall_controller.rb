adecddc14f642bde471f6b48d1bc03d357c1bd26
Grant Petersen-Speelman <gs@easylodge.com.au>
2016-10-22 22:45:06 +0200

    enable remember_me for social logins

--- app/controllers/oneall_controller.rb
+++ app/controllers/oneall_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class OneallController < ApplicationController
+  include Devise::Controllers::Rememberable
   skip_before_action :verify_authenticity_token
 
   def callback
@@ -7,6 +8,7 @@ class OneallController < ApplicationController
     if social_profile.authenticated?
       shopper = Shopper.find_or_create_for_social_profile(social_profile)
       sign_in shopper
+      remember_me shopper
       redirect_to stored_location_for(:shopper) || book_pages_path(PriceBook.default_for_shopper(shopper)),
                   notice: 'Successfully logged in'
     else
