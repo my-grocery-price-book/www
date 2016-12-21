f6bc59fbd6b6183567d13cb10bee197c52b055d8
Grant Petersen-Speelman <grantspeelman@users.noreply.github.com>
2016-12-21 13:28:51 +0200

    can set rollbar enviroment

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -17,13 +17,6 @@ Rollbar.configure do |config|
   # config.person_username_method = "my_username"
   # config.person_email_method = "my_email"
 
-  # config.js_enabled = true
-  # config.js_options = {
-  #   accessToken: ENV['ROLLBAR_ACCESS_TOKEN'],
-  #   captureUncaught: true,
-  #   payload: { environment: Rails.env.to_s }
-  # }
-
   # If you want to attach custom data to all exception and message reports,
   # provide a lambda like the following. It should return a hash.
   # config.custom_data_method = lambda { {:some_key => "some_value" } }
@@ -44,7 +37,7 @@ Rollbar.configure do |config|
   # config.use_async = true
   # Supply your own async handler:
   # config.async_handler = Proc.new { |payload|
-  #  Thread.new { Rollbar.process_payload_safely(payload) }
+  #  Thread.new { Rollbar.process_from_async_handler(payload) }
   # }
 
   # Enable asynchronous reporting (using sucker_punch)
@@ -53,5 +46,12 @@ Rollbar.configure do |config|
   # Enable delayed reporting (using Sidekiq)
   # config.use_sidekiq
   # You can supply custom Sidekiq options:
-  # config.use_sidekiq 'queue' => 'my_queue'
+  # config.use_sidekiq 'queue' => 'default'
+
+  # If you run your staging application instance in production environment then
+  # you'll want to override the environment reported by `Rails.env` with an
+  # environment variable like this: `ROLLBAR_ENV=staging`. This is a recommended
+  # setup for Heroku. See:
+  # https://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
+  config.environment = ENV['ROLLBAR_ENV'] || Rails.env
 end
