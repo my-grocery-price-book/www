617558dcd3b3adf5770a9b6cb84cbf06fff70073
Grant Petersen-Speelman <grantspeelman@users.noreply.github.com>
2016-12-21 14:04:29 +0200

    move country select javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,3 @@
 //= require webpack-bundle
 //= require turbolinks
 //= require_self
-
-jQuery( document ).ready(function( $ ) {
-  // Code using $ as usual goes here.
-  $('#country_select_filter').on('input', function() {
-    input_value = $('#country_select_filter').val().toLowerCase();
-    $('[data-country-item]').each(function() {
-      country_name = $( this ).text().toLowerCase();
-      if(country_name.includes(input_value)) {
-        $( this ).show();
-      } else {
-        $( this ).hide();
-      }
-    });
-  })
-});
