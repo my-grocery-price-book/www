b06ae0b2b669088fd5a405c44b908d2283e248d1
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-08-11 08:15:01 +1000

    yarn my circleci

--- run_cypress.sh
+++ run_cypress.sh
@@ -6,5 +6,6 @@ bundle exec ./bin/rails server -p 5002 -e test &
 sleep 2 # give rails a chance to start up correctly
 
 echo '-- cypress run'
+yarn install
 yarn run cypress install
 yarn run cypress run -P test
