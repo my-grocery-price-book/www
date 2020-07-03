34c6df7427d0c5d0172af38d91fb698f8f007c9a
Grant Petersen-Speelman <grantspeelman@gmail.com>
2020-07-04 06:54:29 +1000

    fix minitest

--- test/libs/region_finder_test.rb
+++ test/libs/region_finder_test.rb
@@ -5,14 +5,14 @@ require 'test_helper'
 describe RegionFinder do
   describe 'first_code' do
     it 'gives first country code' do
-      RegionFinder.instance.first_code.must_equal 'AFG-BDS'
+      _(RegionFinder.instance.first_code).must_equal 'AFG-BDS'
     end
   end
 
   describe 'all' do
     it 'loads all regions' do
       all = RegionFinder.instance
-      all.size.must_equal(3596)
+      _(all.size).must_equal(3596)
     end
   end
 end
