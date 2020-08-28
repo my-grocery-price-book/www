1b4200fa670c2b1729f251bc34e12ba52b1796c0
Grant Petersen-Speelman <grantspeelman@gmail.com>
2020-08-28 21:49:42 +1000

    omniauth condition

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider(
-    :auth0,
-    ENV.fetch('AUTH0_CLIENT_ID'),
-    ENV.fetch('AUTH0_CLIENT_SECRET'),
-    ENV.fetch('AUTH0_DOMAIN'),
-    callback_path: '/auth/auth0/callback',
-    authorize_params: {
-      scope: 'openid email profile'
-    }
-  )
+  if ENV.key?('AUTH0_DOMAIN')
+    provider(
+      :auth0,
+      ENV.fetch('AUTH0_CLIENT_ID'),
+      ENV.fetch('AUTH0_CLIENT_SECRET'),
+      ENV.fetch('AUTH0_DOMAIN'),
+      callback_path: '/auth/auth0/callback',
+      authorize_params: {
+        scope: 'openid email profile'
+      }
+    )
+  end
 end
